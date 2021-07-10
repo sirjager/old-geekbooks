9b84dfc1919914ec775ff4513168de58f941a9c3
Ankur Kumar <jagersir@gmail.com>
2021-07-10 15:58:02 +0530

    .

--- lib/.server/functions/translate/translator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:translator/translator.dart';
-
-final translator = GoogleTranslator();
-
-Future<Translation> translate(String input,
-    {String from = 'auto', String to = "en"}) async {
-  var output = await translator.translate(input, from: from, to: to);
-  return output;
-}
