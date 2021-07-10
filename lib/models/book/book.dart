7283d3d240da52381f7ee0ed40fc0769f2a38b3c
Ankur Kumar <jagersir@gmail.com>
2021-07-10 12:45:45 +0530

    .

--- lib/models/book/book.dart
+++ lib/models/book/book.dart
@@ -1,8 +1,6 @@
 import 'package:geekbooks/backend/constants/api_strings.dart';
 import 'package:hive/hive.dart';
-part 'book.g.dart';
 
-@HiveType(typeId: 2)
 class Book extends HiveObject {
   Book({
     required this.id,
@@ -21,33 +19,19 @@ class Book extends HiveObject {
     required this.tags,
   });
 
-  @HiveField(0)
   final String? id;
-  @HiveField(1)
   final String? md5;
-  @HiveField(2)
   final String? title;
-  @HiveField(3)
   final String? author;
-  @HiveField(4)
   final String? series;
-  @HiveField(5)
   final String? publisher;
-  @HiveField(6)
   final String? year;
-  @HiveField(7)
   final String? language;
-  @HiveField(8)
   final String? pages;
-  @HiveField(9)
   final String? exten;
-  @HiveField(10)
   final String? torrent;
-  @HiveField(11)
   final String? coverURL;
-  @HiveField(12)
   final String? desc;
-  @HiveField(13)
   final List<String>? tags;
 
   factory Book.build(var json) {
---
+++ lib/models/book/encbook.dart
@@ -0,0 +1,63 @@
+import 'package:geekbooks/backend/constants/api_strings.dart';
+import 'package:geekbooks/export/export.dart';
+import 'package:hive/hive.dart';
+part 'encbook.g.dart';
+
+@HiveType(typeId: 2)
+class EncBook extends HiveObject {
+  EncBook({
+    required this.id,
+    required this.md5,
+    required this.title,
+    required this.author,
+    required this.series,
+    required this.publisher,
+    required this.year,
+    required this.language,
+    required this.pages,
+    required this.exten,
+    required this.torrent,
+    required this.coverURL,
+    required this.desc,
+    required this.tags,
+  });
+
+  @HiveField(0)
+  final String? id;
+  @HiveField(1)
+  final String? md5;
+  @HiveField(2)
+  final String? title;
+  @HiveField(3)
+  final String? author;
+  @HiveField(4)
+  final String? series;
+  @HiveField(5)
+  final String? publisher;
+  @HiveField(6)
+  final String? year;
+  @HiveField(7)
+  final String? language;
+  @HiveField(8)
+  final String? pages;
+  @HiveField(9)
+  final String? exten;
+  @HiveField(10)
+  final String? torrent;
+  @HiveField(11)
+  final String? coverURL;
+  @HiveField(12)
+  final String? desc;
+  @HiveField(13)
+  final List<String>? tags;
+
+  Future<EncBook?> buildEnc(Book json) async {
+
+
+
+
+
+
+    
+  }
+}
